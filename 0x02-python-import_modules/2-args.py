de2e032d5d959b148eb50a4d661199ee3bcef33a
tech-hq <ebubechi.anyanwu@yahoo.com>
2023-05-14 09:51:37 -0700

    Last changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     elif lenArgv == 1:
         print("1 argument:")
     else:
-        print("{} arguments".format(lenArgv))
+        print("{} arguments:".format(lenArgv))
     for i in range(1, lenArgv + 1):
         print("{}: {}".format(i, sys.argv[i]))
