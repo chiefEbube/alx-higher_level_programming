4c553d2e782ac4a5f75763a2108028f5dc8c298d
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-09-07 16:17:01 +0100

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,11 +5,9 @@
 class Square:
     """Representation of a square"""
 
-
     def __init__(self, size):
         """Initializing this square class
         Args: size - represnets the size of the square defined
         """
-
 
         self.__size = size
