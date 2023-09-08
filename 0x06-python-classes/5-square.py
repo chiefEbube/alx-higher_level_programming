c1c598da4a30b660d61b3aa40723aa946ab8dfd5
chiefebube <ebubechi.anyanwu@yahoo.com>
2023-09-08 03:40:08 -0700

    task 6

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Representation of a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves size of square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets size of square"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints in stdout the square with
+        the character #
+        """
+
+        if self.__size == 0:
+            print()
+
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
