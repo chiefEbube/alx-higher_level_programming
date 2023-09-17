a16fbac76961a180e424558a2445dcabbb94bafd
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-09-17 16:29:50 +0100

    fixed indentation error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,30 +17,30 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """retrieves the width attribute"""
-            return self.__width
+    @property
+    def width(self):
+        """retrieves the width attribute"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """sets the width attribute"""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """sets the width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """retrieves the height attribute"""
-            return self.__height
+    @property
+    def height(self):
+        """retrieves the height attribute"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """sets the height attribute"""
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """sets the height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
